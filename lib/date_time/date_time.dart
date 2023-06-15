da79753b682197ab39f22886b6599feb8d18eba4
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-06-15 15:16:13 +0300

    add: date time formatter

---
+++ lib/date_time/date_time.dart
@@ -0,0 +1,15 @@
+import 'package:intl/intl.dart';
+
+// save request query with isar
+extension DateTimeX on DateTime {
+  String formatDate([String? locale]) =>
+      DateFormat('yyyy/MM/dd', locale).format(this);
+
+  String formatTime([String? locale]) =>
+      DateFormat('hh:mm a', locale).format(this);
+
+  String formatDOW([String? locale]) => DateFormat('EEEE', locale).format(this);
+
+  String format([String? locale]) =>
+      DateFormat('yyyy/MM/dd hh:mm', locale).format(this);
+}

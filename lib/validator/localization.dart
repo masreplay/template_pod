5bed950d643ba842c66e09fe3720036704db9729
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-06-15 15:16:21 +0300

    fix: validation localization

--- lib/validator/localization.dart
+++ lib/validator/localization.dart
@@ -1,32 +1,30 @@
 import 'package:form_validator/form_validator.dart';
-import 'package:starter/common_lib.dart';
+import 'package:ekyc/common_lib.dart';
 
 class FormValidatorLocaleApp implements FormValidatorLocale {
   final BuildContext context;
   const FormValidatorLocaleApp(this.context);
 
   @override
-  String name() => 'ar';
+  String name() => context.l10n.localeName;
 
   @override
-  String minLength(String v, int n) =>
-      'The field must be at least $n characters long';
+  String minLength(String v, int n) => context.l10n.validatorMinLength(n);
 
   @override
-  String maxLength(String v, int n) =>
-      'The field must be at most $n characters long';
+  String maxLength(String v, int n) => context.l10n.validatorMaxLength(n);
 
   @override
-  String email(String v) => 'The field is not a valid email address';
+  String email(String v) => context.l10n.validatorEmail;
 
   @override
-  String phoneNumber(String v) => 'The field is not a valid phone number';
+  String phoneNumber(String v) => context.l10n.validatorPhoneNumber;
 
   @override
-  String required() => 'The field is required';
+  String required() => context.l10n.validatorRequired;
 
   @override
-  String url(String v) => 'The field is not a valid URL address';
+  String url(String v) => context.l10n.validatorUrl;
 
   @override
   String ip(String v) => throw UnimplementedError();
